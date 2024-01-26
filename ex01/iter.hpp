70d0d94771980de7bf38c21d6607f697fbd0a174
SeungMinLee <seunlee2@student.42seoul.kr>
2024-01-26 16:21:47 +0900

    add Tfunc

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,10 +3,10 @@
 
 #include <iostream>
 
-template <typename T>
-void iter(T* array, std::size_t len, void (*f)(T&)) {
+template <typename T, typename Tfunc>
+void iter(T* array, std::size_t len, Tfunc func) {
 	for (std::size_t i = 0; i < len; i++) {
-		f(array[i]);
+		func(array[i]);
 	}
 }
 
